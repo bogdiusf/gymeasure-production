68d7237235fb0c1abc2331a18690813e078acf54
Filip <bogdiusf@gmail.com>
2021-08-14 17:05:06 +0300

    Fix: started changing login/sign up pages design

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -38,41 +38,39 @@ export default function Login() {
     }
 
     return (
-        <FormContainer
-            title="Sign in"
-            error={error}
-            footer={<FormFooter value1="Don't have an account?" value2="Sign up" path="/signup" />}
-        >
-            <Form onSubmit={handleLogin}>
-                <Form.Group id="email" className="mt-2">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                        placeholder="Please enter your e-mail"
-                    />
-                </Form.Group>
+        <>
+            <FormContainer title="Sign in" error={error} footer={<FormFooter value1="Don't have an account?" value2="Sign up" path="/signup" />}>
+                <Form onSubmit={handleLogin}>
+                    <Form.Group id="email" className="mt-2">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                            placeholder="Please enter your e-mail"
+                        />
+                    </Form.Group>
 
-                <Form.Group id="password" className="mt-2">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        required
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Please enter your password"
-                    ></Form.Control>
-                </Form.Group>
-                <Button type="submit" className="text-center w-100 mt-4" disabled={loading}>
-                    Log in
-                </Button>
-            </Form>
+                    <Form.Group id="password" className="mt-2">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            required
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="Please enter your password"
+                        ></Form.Control>
+                    </Form.Group>
+                    <Button type="submit" className="text-center w-100 mt-4" disabled={loading}>
+                        Log in
+                    </Button>
+                </Form>
 
-            <div className="w-100 text-center mt-3">
-                <Link to="/forgot-password">Forgot password?</Link>
-            </div>
-        </FormContainer>
+                <div className="w-100 text-center mt-3">
+                    <Link to="/forgot-password">Forgot password?</Link>
+                </div>
+            </FormContainer>
+        </>
     )
 }
--- src/components/shared/FormContainer.js
+++ src/components/shared/FormContainer.js
@@ -1,20 +1,43 @@
 import React from 'react'
-import { Card, Alert, Container } from 'react-bootstrap'
+import { Card, Alert } from 'react-bootstrap'
+import styled from 'styled-components'
+import welcomePic from '../../images/not-logged-pic.jpg'
 
 const FormContainer = ({ children, title, error, message, footer }) => (
-    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-            <Card>
+    <StyledBody>
+        <BackgroundImage img={welcomePic} />
+        <Container>
+            <CredentialsWrapper style={{ height: '100vh' }}>
                 <Card.Body>
                     <h2 className="text-center mb-4">{title}</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     {message && <Alert variant="success">{message}</Alert>}
                     {children}
+                    {footer}
                 </Card.Body>
-            </Card>
-            {footer}
-        </div>
-    </Container>
+            </CredentialsWrapper>
+        </Container>
+    </StyledBody>
 )
 
 export default FormContainer
+
+const StyledBody = styled.div`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+`
+const Container = styled.div`
+    flex: 1;
+    min-width: 400px;
+`
+const BackgroundImage = styled.div`
+    height: 100vh;
+    background-image: url(${(props) => props.img});
+    background-size: cover;
+    flex: 1.5;
+`
+const CredentialsWrapper = styled.div`
+    background: white;
+`

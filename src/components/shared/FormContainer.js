aef2bf03129be4c1d7a0602f9e65714852f29b2e
Filip <bogdiusf@gmail.com>
2021-08-14 17:47:23 +0300

    Fix: minor changes login page

--- src/components/shared/FormContainer.js
+++ src/components/shared/FormContainer.js
@@ -1,22 +1,21 @@
 import React from 'react'
-import { Card, Alert } from 'react-bootstrap'
+import { Alert } from 'react-bootstrap'
 import styled from 'styled-components'
 import welcomePic from '../../images/not-logged-pic.jpg'
 
 const FormContainer = ({ children, title, error, message, footer }) => (
     <StyledBody>
         <BackgroundImage img={welcomePic} />
-        <Container>
-            <CredentialsWrapper style={{ height: '100vh' }}>
-                <Card.Body>
-                    <h2 className="text-center mb-4">{title}</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    {message && <Alert variant="success">{message}</Alert>}
-                    {children}
-                    {footer}
-                </Card.Body>
-            </CredentialsWrapper>
-        </Container>
+
+        <CredentialsWrapper>
+            <div>
+                <h2 className="text-center mb-4">{title}</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                {message && <Alert variant="success">{message}</Alert>}
+                {children}
+                {footer}
+            </div>
+        </CredentialsWrapper>
     </StyledBody>
 )
 
@@ -26,18 +25,29 @@ const StyledBody = styled.div`
     height: 100vh;
     width: 100%;
     display: flex;
-    justify-content: flex-end;
-`
-const Container = styled.div`
-    flex: 1;
-    min-width: 400px;
 `
 const BackgroundImage = styled.div`
     height: 100vh;
     background-image: url(${(props) => props.img});
     background-size: cover;
     flex: 1.5;
+
+    @media screen and (max-width: 450px) {
+        display: none;
+    }
 `
 const CredentialsWrapper = styled.div`
     background: white;
+    height: '100vh';
+    flex: 1;
+    width: 100%;
+    min-width: 375px;
+    padding: 35px;
+    border: 2px solid red;
+    display grid;
+    place-items: center;
+
+    > div{
+        border: 1px solid blue;
+    }
 `
